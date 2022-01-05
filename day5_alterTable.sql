a2f70ca8f7373ae18f25cdd880e4eabe69313afc
Ferhat <fcetinerr@gmail.com>
2022-01-06 00:18:20 +0300

    existOrderLimit,GroupBy

--- day5_alterTable.sql
+++ day5_alterTable.sql
@@ -64,5 +64,12 @@ rename column ulke_isim to ulke_adi;
   alter table isciler
   modify ulke_adi varchar(30) not null;
   
+ -- maas limit kisitlamasi ekle
+  
+  alter table isciler add constraint  check (maas >= 3500);
+     -- Maas alt limit kisitlamasi atadik.
+    -- kisitlama atadiktan sonra maasin 3500 altinda olmasi sebebiyle
+    -- alttaki veriyi giremeyiz 
+    INSERT INTO isciler VALUES(123452310, 'Hatice Sahin', 'Bursa', 3000, null);
   
     
